aeb64cc607b46f77ee93716e50866d8a0654a0b7
Marek Lewandowski <lewymarek1221@gmail.com>
2025-12-10 14:42:39 +0100

    add archive functionality for completed tasks in task manager

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,15 +1,16 @@
-import { Component, inject, OnInit} from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Task } from '../task';
 import { TasksService } from '../TasksService';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './tasks.html',
-  styleUrl: './tasks.css'
+  styleUrl: './tasks.css',
 })
 export class Tasks implements OnInit {
   tasks: Task[] = [];
@@ -25,7 +26,7 @@ export class Tasks implements OnInit {
       },
       error: (err) => {
         console.error('Błąd:', err);
-      }
+      },
     });
   }
 
@@ -49,8 +50,8 @@ export class Tasks implements OnInit {
       error: (err) => {
         this.errorMessage = 'Błąd serwera!';
         console.error('Błąd:', err);
-      }
-    })
+      },
+    });
   }
 
   handleChange(task: Task): void {
@@ -60,8 +61,34 @@ export class Tasks implements OnInit {
       },
       error: (err) => {
         console.error('Błąd:', err);
-        this.errorMessage = 'Nie udało się zapisać zmiany statusu.'
-      }
-    })
+        this.errorMessage = 'Nie udało się zapisać zmiany statusu.';
+      },
+    });
+  }
+
+  archiveCompleted(): void {
+    // Znalezienie zadań do archiwizacji
+    const tasksToArchive = this.tasks.filter((t) => t.completed);
+
+    if (tasksToArchive.length === 0) {
+      return;
+    }
+
+    // Lokalne przygotowanie zadań do archiwizacji (zawiera obiekty Observable)
+    const requests = tasksToArchive.map((task) => {
+      task.archived = true;
+      return this.tasksService.put(task);
+    });
+
+    // Wysłanie wszystkich żądań archiwizacji
+    forkJoin(requests).subscribe({
+      next: () => {
+        console.log('Zarchiwizowano zadania');
+        this.ngOnInit(); // Odświeżenie listy zadań
+      },
+      error: (err) => {
+        console.error('Błąd:', err);
+      },
+    });
   }
 }
